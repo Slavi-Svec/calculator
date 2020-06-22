43e52f818ef0e92275cd1bd9fc9462fafcdefceb
izaak svec <slavi.svec@gmail.com>
2020-06-22 13:21:15 +1000

    Updated AC to clearn numbers

--- main.js
+++ main.js
@@ -1,19 +1,30 @@
 const buttons = document.getElementsByClassName("calc-btn")
 let screen = document.getElementsByClassName("calc-display-val")[0]
 const equals = document.getElementById("calc-equals")
-const sums = []
+const clear = document.getElementById("calc-clear")
+let sums = []
 
 for (let i = 0; i < buttons.length; i++){
   buttons[i].addEventListener('click', event => {
+    const shouldNotRenderZero = buttons.length === 1 && buttons[0] === 0
     const button = buttons[i].innerText
     if (button !== '=') {
       sums.push(button)
+      console.log(sums)
     }
     screen.innerText = sums.join("")
   })
 }
 
 equals.addEventListener("click", function(){
+  // console.log(eval(sums.join('')))
+  console.log(sums)
   screen.innerText = eval(sums.join(''))
 })
+
+clear.addEventListener("click", function(){
+  screen.innerText = '0'
+  sums = []
+  console.log(sums)
+});
 
