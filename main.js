1a6e6d51eecd3e08b67b54b5e52b695294c7f5ec
izaak svec <slavi.svec@gmail.com>
2020-06-24 07:28:53 +1000

    added regex to match decimal place

--- main.js
+++ main.js
@@ -2,33 +2,38 @@ const buttons = document.getElementsByClassName("calc-btn")
 let screen = document.getElementsByClassName("calc-display-val")[0]
 const equals = document.getElementById("calc-equals")
 const clear = document.getElementById("calc-clear")
-let sums = []
+let display = []
 
 for (let i = 0; i < buttons.length; i++){
   buttons[i].addEventListener('click', () => {
     const button = buttons[i].innerText
-    const shouldNotRenderZero = sums[0] === '0'
+    const shouldNotRenderZero = display[0] === '0'
       && button === '0'
-      && sums.length < 2
+      && display.length < 2
+    const hasDuplicates = (/([.]).*?\1/).test(display)
+
+    console.log('display=', display, 'hasDuplicates=', hasDuplicates)
 
     if (shouldNotRenderZero || button === '=') {
-      console.log('button =', button)
       return
     }
-    sums.push(button)
-    screen.innerText = sums.join("")
+    display.push(button)
+    screen.innerText = display.join("")
   })
 }
 
 equals.addEventListener("click", function(){
-  console.log(eval(sums.join('')))
-  console.log(sums)
-  screen.innerText = eval(sums.join(''))
+  screen.innerText = eval(display.join(''))
 })
 
 clear.addEventListener("click", () => {
   screen.innerText = '0'
-  sums = []
-  console.log(sums)
+  display = []
 });
 
+
+
+
+const str= [".", "."]
+var hasDuplicates = (/([.]).*?\1/).test(str)
+console.log("repeating string ", hasDuplicates)
