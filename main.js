02b5c6000a48fc357e5ff43c93f6a1999cec0f38
izaak svec <slavi.svec@gmail.com>
2020-06-23 14:33:29 +1000

    Fixed decimal points and zero

--- main.js
+++ main.js
@@ -5,24 +5,28 @@ const clear = document.getElementById("calc-clear")
 let sums = []
 
 for (let i = 0; i < buttons.length; i++){
-  buttons[i].addEventListener('click', event => {
-    const shouldNotRenderZero = buttons.length === 1 && buttons[0] === 0
+  buttons[i].addEventListener('click', () => {
     const button = buttons[i].innerText
-    if (button !== '=') {
-      sums.push(button)
-      console.log(sums)
+    const shouldNotRenderZero = sums[0] === '0'
+      && button === '0'
+      && sums.length < 2
+
+    if (shouldNotRenderZero || button === '=') {
+      console.log('button =', button)
+      return
     }
+    sums.push(button)
     screen.innerText = sums.join("")
   })
 }
 
 equals.addEventListener("click", function(){
-  // console.log(eval(sums.join('')))
+  console.log(eval(sums.join('')))
   console.log(sums)
   screen.innerText = eval(sums.join(''))
 })
 
-clear.addEventListener("click", function(){
+clear.addEventListener("click", () => {
   screen.innerText = '0'
   sums = []
   console.log(sums)
